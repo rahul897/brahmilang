5f5cf3d7c3d70cced9ed51251af23fd547d32a34
rahul <rahul.iragavarapu@gmail.com>
2023-07-12 23:41:55 +0530

    builtin methods

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import string
 
 DIGITS = '0123456789'
@@ -609,7 +610,7 @@ class Parser:
                                                   "Expected ="))
         res.register_advance()
         self.advance()
-        expr = res.register(self.arith_expr())
+        expr = res.register(self.statement())
         if res.error: return res
         return res.success(VarAssignNode(var_name, expr, define))
 
@@ -915,44 +916,143 @@ class Number(Value):
 
     def __repr__(self):
         return f'{self.value}'
+Number.null = Number(0)
+Number.false = Number(0)
+Number.true = Number(1)
 
-
-class Function(Value):
-    def __init__(self, name, body, arg_names):
+class BaseFunction(Value):
+    def __init__(self, name):
         super().__init__()
-        self.name = name or "anonymous"
+        self.name = name or "<anonymous>"
+
+    def generate_new_context(self):
+        new_context = Context(self.name, self.context, self.pos_start)
+        new_context.symbol_table = SymbolTable(new_context.parent.symbol_table)
+        return new_context
+
+    def check_args(self, arg_names, args):
+        res = RTResult()
+        if len(args) != len(arg_names):
+            return res.failure(RTError(self.pos_start, self.pos_end,
+                                       f"{len(args)} args passed -> need {len(arg_names)} args"
+                                       , self.context))
+
+        return res.success(None)
+
+    def populate_args(self, arg_names, args, exec_ctx):
+        for i in range(len(args)):
+            arg_name = arg_names[i]
+            arg_value = args[i]
+            arg_value.set_context(exec_ctx)
+            exec_ctx.symbol_table.set(arg_name, arg_value)
+
+    def check_and_populate_args(self, arg_names, args, exec_ctx):
+        res = RTResult()
+        res.register(self.check_args(arg_names, args))
+        if res.error: return res
+        self.populate_args(arg_names, args, exec_ctx)
+        return res.success(None)
+
+
+class Function(BaseFunction):
+    def __init__(self, name, body, arg_names):
+        super().__init__(name)
         self.body = body
         self.arg_names = arg_names
 
     def execute(self, args):
         res = RTResult()
         interpreter = Interpreter()
-        new_context = Context(self.name, self.context, self.pos_start)
-        new_context.symbol_table = SymbolTable(new_context.parent.symbol_table)
+        exec_ctx = self.generate_new_context()
 
-        if len(args) != len(self.arg_names):
-            return res.failure(RTError(self.pos_start, self.pos_end,
-                                       f"{len(args)} args passed -> need {len(self.arg_names)} args"
-                                       , self.context))
+        res.register(self.check_and_populate_args(self.arg_names, args, exec_ctx))
+        if res.error: return res
 
-        for i in range(len(args)):
-            arg_name = self.arg_names[i]
-            arg_value = args[i]
-            arg_value.set_context(new_context)
-            new_context.symbol_table.set(arg_name, arg_value)
-
-        value = res.register(interpreter.visit(self.body, new_context))
+        value = res.register(interpreter.visit(self.body, exec_ctx))
         if res.error: return res
         return res.success(value)
 
     def copy(self):
         copy = Function(self.name, self.body, self.arg_names)
-        copy.set_pos(self.pos_start, self.pos_end)
         copy.set_context(self.context)
+        copy.set_pos(self.pos_start, self.pos_end)
         return copy
 
     def __repr__(self):
-        return f"<function {self.name}"
+        return f"<function {self.name}>"
+
+
+class BuiltInFunction(BaseFunction):
+    def __init__(self, name):
+        super().__init__(name)
+
+    def execute(self, args):
+        res = RTResult()
+        exec_ctx = self.generate_new_context()
+
+        method_name = f'execute_{self.name}'
+        method = getattr(self, method_name, self.no_visit_method)
+
+        res.register(self.check_and_populate_args(method.arg_names, args, exec_ctx))
+        if res.error: return res
+
+        return_value = res.register(method(exec_ctx))
+        if res.error: return res
+        return res.success(return_value)
+
+    def no_visit_method(self, node, context):
+        raise Exception(f'No execute_{self.name} method defined')
+
+    def copy(self):
+        copy = BuiltInFunction(self.name)
+        copy.set_context(self.context)
+        copy.set_pos(self.pos_start, self.pos_end)
+        return copy
+
+    def __repr__(self):
+        return f"<built-in function {self.name}>"
+
+    def execute_print(self, exec_ctx):
+        print(str(exec_ctx.symbol_table.get('value')))
+        return RTResult().success(Number.null)
+
+    execute_print.arg_names = ['value']
+
+    def execute_print_ret(self, exec_ctx):
+        return RTResult().success(String(str(exec_ctx.symbol_table.get('value'))))
+
+    execute_print_ret.arg_names = ['value']
+
+    def execute_input(self, exec_ctx):
+        text = input()
+        return RTResult().success(String(text))
+
+    execute_input.arg_names = []
+
+    def execute_input_int(self, exec_ctx):
+        while True:
+            text = input()
+            try:
+                number = int(text)
+                break
+            except ValueError:
+                print(f"'{text}' must be an integer. Try again!")
+        return RTResult().success(Number(number))
+
+    execute_input_int.arg_names = []
+
+    def execute_clear(self, exec_ctx):
+        os.system('cls' if os.name == 'nt' else 'cls')
+        return RTResult().success(Number.null)
+    execute_clear.arg_names = []
+
+
+
+BuiltInFunction.print = BuiltInFunction("print")
+BuiltInFunction.print_ret = BuiltInFunction("print_ret")
+BuiltInFunction.input = BuiltInFunction("input")
+BuiltInFunction.input_int = BuiltInFunction("input_int")
+BuiltInFunction.clear = BuiltInFunction("clear")
 
 
 class RTResult:
@@ -1053,7 +1153,7 @@ class Interpreter:
         if value == None:
             return res.failure(RTError(node.pos_start, node.pos_end,
                                        f'{var_name} is not defined', context))
-        value = value.copy().set_pos(node.pos_start, node.pos_end)
+        value = value.copy().set_pos(node.pos_start, node.pos_end).set_context(context)
         return res.success(value)
 
     def visit_VarAssignNode(self, node, context):
@@ -1127,7 +1227,7 @@ class Interpreter:
 
         return_value = res.register(value_to_call.execute(args))
         if res.error: return res
-
+        return_value = return_value.copy().set_pos(node.pos_start,node.pos_end).set_context(context)
         return res.success(return_value)
 
 
@@ -1161,7 +1261,11 @@ global_symbol_table = SymbolTable()
 global_symbol_table.set("null", Number(0))
 global_symbol_table.set("false", Number(0))
 global_symbol_table.set("true", Number(1))
-
+global_symbol_table.set("print", BuiltInFunction.print)
+global_symbol_table.set("print_ret", BuiltInFunction.print_ret)
+global_symbol_table.set("input", BuiltInFunction.input)
+global_symbol_table.set("input_int", BuiltInFunction.input_int)
+global_symbol_table.set("clear", BuiltInFunction.clear)
 
 def run(fn, text):
     lexer = Lexer(fn, text)
@@ -1173,7 +1277,7 @@ def run(fn, text):
     if ast.error: return None, ast.error
 
     interpreter = Interpreter()
-    context = Context("root")
+    context = Context("<program>")
     context.symbol_table = global_symbol_table
     result = interpreter.visit(ast.node, context)
 
